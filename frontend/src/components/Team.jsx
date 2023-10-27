d3af7cba831047e74ea23871597ca98c2b601eaf
MugoDom <njuemugodominic@gmail.com>
2023-10-27 12:05:03 +0300

    chore: delete unused files

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -1,71 +1,3 @@
-<<<<<<< HEAD:moonex/src/components/team/Team.jsx
-import React from "react";
-
-const teamContent = [
-  {
-    delayAnimation: "0",
-    img: "team-1",
-    title: "Dominic N Mugo",
-    designation: "CEO Founder",
-  },
-  {
-    delayAnimation: "100",
-    img: "team-2",
-    title: "Kevin Odhiambo",
-    designation: "Lead - Strategy and Innovation",
-  },
-  {
-    delayAnimation: "200",
-    img: "team-3",
-    title: "Yvonne Yano",
-    designation: "Lead - Marketing and Advertising",
-  },
-  {
-    delayAnimation: "300",
-    img: "team-4",
-    title: "Raymond Munene",
-    designation: "Creative and Media Producer",
-  },
-];
-
-const Team = () => {
-  return (
-    <>
-      {teamContent.map((val, i) => (
-        <div
-          className="ptf-animated-block"
-          data-aos="fade"
-          data-aos-delay={val.delayAnimation}
-          key={i}
-        >
-          {/* <!--Team Member--> */}
-          <div className="ptf-team-member ptf-team-member--has-effect">
-            <div className="ptf-team-member__avatar">
-              <div className="shadow-effect"></div>
-              <a href="#">
-                {" "}
-                <img
-                  src={`assets/img/root/team/${val.img}.png`}
-                  alt={val.title}
-                  loading="lazy"
-                />
-              </a>
-            </div>
-            <div className="ptf-team-member__content">
-              <h6 className="ptf-team-member__name">
-                <a href="#">{val.title}</a>
-              </h6>
-              <p className="ptf-team-member__function">{val.designation}</p>
-            </div>
-          </div>
-        </div>
-      ))}
-    </>
-  );
-};
-
-export default Team;
-=======
 import React from "react";
 import {Link} from "react-router-dom";
 
@@ -133,4 +65,3 @@ const Team = () => {
 };
 
 export default Team;
->>>>>>> origin/main:frontend/src/components/Team.jsx
